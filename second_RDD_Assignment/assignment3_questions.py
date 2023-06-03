644da28fda1f5174c6aa97d679cbe92d95695236
sumit sahni <sumitsahni76@gmail.com>
2023-06-03 14:58:36 -0500

    scala project example

---
+++ second_RDD_Assignment/assignment3_questions.py
@@ -0,0 +1,66 @@
+
+In this assignment you will analysis an Olympics dataset using PySpark.
+
+# Olympics Dataset 
+This data is a publicly available. The ***olympix_data.csv*** file is in the Blackboard.
+
+# Dataset Description
+
+This data set consists of the following fields:
+
+***Athlete [0]***: Name of the athlete
+
+***Age [1]***: Age of the athlete
+
+***Country [2]***: The name of the country participating in Olympics
+
+***Year [3]***: The year in which Olympics is conducted
+
+***Closing Date [4]***: Closing date of Olympics
+
+***Sport [5]***: Sports name
+
+***Gold Medals [6]***: No. of gold medals
+
+***Silver Medals [7]***: No. of silver medals
+
+***Bronze Medals [8]***: No. of bronze medals
+
+***Total Medals [9]***: Total no. of medals
+"""
+# please use pyspark RDD to do that
+
+# create entry points to spark
+
+"""## Q1: Write PySpark code to read olympix_data.csv file"""
+
+#read the data
+
+"""## Q2: Write PySpark code to print the Olympic Sports/games in the dataset."""
+
+
+
+"""![alt text](https://)## Q3: Write PySpark code to plot the total number of medals in  each Olympic Sport/game. Sort the result based on the the total number of medals."""
+
+
+
+"""### Q4: Find the total number of medals won by each country in swimming.
+
+
+
+"""### Q5: Find the total number of medals won by each country in Skeleton."""
+
+
+
+"""### Q6 Find the number of medals that the US won yearly."""
+
+
+
+"""### Q7 Find the total number of medals won by each country."""
+
+
+
+"""### Q8 Who was the oldest athletic in the olympics? whcih country was he/she from?"""
+
+
+
